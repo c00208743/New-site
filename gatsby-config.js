c58397ec6618ffefd5b0ac729e673291af570b4a
Jamie <c00208743@itcarlow.ie>
2018-04-10 14:46:35 +0100

    deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-tagmanager`,
       options: {
-        id: "UA-117214420-1",
+        id: "GTM-KRRTV7C",
         // Include GTM in development.
         // Defaults to false meaning GTM will only be loaded in production.
         includeInDevelopment: false,
